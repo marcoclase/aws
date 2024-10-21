41af2afee393267cc38fcab4545e19c95e2627ea
marcoclase <47568193+marcoclase@users.noreply.github.com>
2024-10-21 13:51:56 +0200

    Update ec2-user-data-al2-web.sh

--- ec2-user-data-al2-web.sh
+++ ec2-user-data-al2-web.sh
@@ -2,8 +2,8 @@
 # Instalar Apache Web Server y PHP 
 yum install -y httpd mysql php 
 # Descargar web de ejemplo
-wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-100-ACCLFO-2-102668/2-lab2-vpc/s3/lab-app.zip 
-unzip lab-app.zip -d /var/www/html/ 
+wget https://github.com/marcoclase/aws/blob/main/ejemplo-web-tienda.zip
+unzip ejemplo-web-tienda.zip -d /var/www/html/ 
 # Levantar servidor web
 chkconfig httpd on 
 service httpd start
