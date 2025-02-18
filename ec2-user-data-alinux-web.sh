39aff826928dc244a41285738fb8903dd9a0e025
marcoclase <47568193+marcoclase@users.noreply.github.com>
2025-02-19 00:01:11 +0100

    Actualizado

--- ec2-user-data-alinux-web.sh
+++ ec2-user-data-alinux-web.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# Actualizar permisos
 yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
+yum install httpd -y
+# Modificar usuarios y permisos
 usermod -a -G apache ec2-user
 chown -R ec2-user:apache /var/www
 chmod 775 /var/www
 echo "<html><h1>Hola!</h1></html>" > /var/www/html/index.html
 chown ec2-user:apache /var/www/html/index.html
+# Levantar servicio
+systemctl start httpd
+systemctl enable httpd
