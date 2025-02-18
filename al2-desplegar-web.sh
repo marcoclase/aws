f4dbf5ad505045a0617a9ba6c92757e4f55a5939
marcoclase <47568193+marcoclase@users.noreply.github.com>
2025-02-18 23:58:56 +0100

    Delete al2-desplegar-web.sh

--- al2-desplegar-web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Instalar Apache Web Server y PHP 
-yum install -y httpd mysql php 
-# Descargar web de ejemplo
-wget http://my-url/lab-app.zip 
-unzip lab-app.zip -d /var/www/html/ 
-# Levantar servidor web
-chkconfig httpd on 
-service httpd start
